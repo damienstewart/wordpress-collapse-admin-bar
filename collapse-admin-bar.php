cc6a6c239481f21a79c3bd99d02ac4876d76531a
Damien Stewart <damienstewart.me@gmail.com>
2019-07-23 13:55:46 -0700

    v1.1
    Changed doctype declaration bug

--- collapse-admin-bar.php
+++ collapse-admin-bar.php
@@ -1,15 +1,14 @@
 <?php
 /*
 Plugin Name: Collapse Admin Bar
-Plugin URI:  http://damienstewart.me/
-Description: Allows you to show and hide the admin menu bar from the front end of the website
-Version:     1.0
-Author:      Damien Stewart
-Author URI:  http://damienstewart.me/
-License:     GPL3
-License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+Plugin URI: https://damienstewart.me/
+Description: Allows you to collapse the admin bar on your websites front-end
+Version: 1.1
+Author: Damien Stewart
+Author URI: https://damienstewart.me/
+Text Domain: collapse-admin=bar
 */
-function collapse_admin_bar_script() {   
+function collapse_admin_bar() {   
     wp_enqueue_script( 'collapse_admin_bar_js', plugin_dir_url( __FILE__ ) . 'js/main.js' );
 }
-add_action('wp_enqueue_scripts', 'collapse_admin_bar_script', 99999);
+add_action('wp_enqueue_scripts', 'collapse_admin_bar', 99999);
--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ jQuery.noConflict();
 		if( $('body').hasClass('admin-bar') && !$('body').hasClass('wp-admin') ) {
 			$('html').attr('style', 'margin-top: 0 !important;');
 			$('#wpadminbar').attr('style', 'display: none !important;');
-			$('body').prepend('<div id="admin-toggle" style="cursor: pointer; z-index: 99999999999 !important; width: 10px; height: 10px; position: fixed; left: 0; top: 0; background-color: #333;"><i style="color: white !important; width: 12px; font-size: 10px; margin-left: -1px;" class="admin-toggle-plus fa fa-plus"></i></div>');
+			$('body').prepend('<div id="admin-toggle" style="cursor: pointer; z-index: 99999999999 !important; width: 10px; height: 10px; position: fixed; left: 0; top: 0; background-color: #333;"><i style="color: white !important; width: 12px; font-size: 10px; margin-left: -1px; top: 0 !important; position: absolute !important;" class="admin-toggle-plus fa fa-plus"></i></div>');
 			
 			$('#admin-toggle').click(function(){
 				$(this).toggleClass('open');
@@ -16,11 +16,11 @@ jQuery.noConflict();
 				if( $('#admin-toggle').hasClass('open') ) {
 					$('#wpadminbar').attr('style', 'display: fixed !important;');
 					$('html').attr('style', 'margin-top: 32px !important;');
-					$('.admin-toggle-plus').attr('style', 'color: white !important; width: 12px; font-size: 10px; margin-left: -1px; height: 2px; overflow: hidden; margin-top: 4px; line-height: 0.3;');
+					$('.admin-toggle-plus').attr('style', 'color: white !important; width: 12px; font-size: 10px; margin-left: -1px; height: 2px; overflow: hidden; margin-top: 4px; line-height: 0.3; top: 0 !important; position: absolute !important;');
 			} else {
 				$('html').attr('style', 'margin-top: 0 !important;');
 				$('#wpadminbar').attr('style', 'display: none !important;');
-				$('.admin-toggle-plus').attr('style', 'color: white !important; width: 12px; font-size: 10px; margin-left: -1px;');
+				$('.admin-toggle-plus').attr('style', 'color: white !important; width: 12px; font-size: 10px; margin-left: -1px; top: 0 !important; position: absolute !important;');
 			}
 			}
 		}
